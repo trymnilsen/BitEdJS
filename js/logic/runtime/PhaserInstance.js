1951d32c366f0a6bf4c6665b165f604e2d8d0266
Trym Nilsen <trym.nilsen@gmail.com>
2014-07-11 01:54:22 +0200

    Phasor event functions

--- js/logic/runtime/PhaserInstance.js
+++ js/logic/runtime/PhaserInstance.js
@@ -1,24 +1,35 @@
 'use strict';
 
 define([
-  'phaser'
-], function(phaser){
+  'phaser',
+  'underscore'
+], function(phaser,_){
 
-  var TabView = {
+  var PhaserInstance = {
 
-    game : null;
+    game : null,
 
     attatch: function(domID) {
-      if(domID=='' || domID == undefined)
+      if(domID === '' || domID === undefined)
       {
         console.log('phaser attach id not defined');
         return;
       }
-      this.game = new phaser.Game(600, 480, Phaser.AUTO, domID);
+      this.game = new phaser.Game(600, 480, phaser.AUTO, domID, 
+          {
+            preload: _.bind(this.preload,this),
+            create: _.bind(this.create,this)
+          });
+    },
+    preload: function(){
+
+    },
+    create: function(){
+
     }
 
-  });
+  };
 
-  return TabView;
+  return PhaserInstance;
   
 });
