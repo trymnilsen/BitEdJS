4df466a4afa0a1e5f93b83cd275d3fdbb17d9fb2
Trym Nilsen <niltry10@nith.no>
2014-08-13 23:39:56 +0200

    added components

--- js/logic/editor/Editor.js
+++ js/logic/editor/Editor.js
@@ -16,6 +16,6 @@ function (
     
     return {
         assetPipeline : editorGenericPipeline,
-
+        
     };
 });
--- js/views/editor/sidebar/properties/components/PropertiesComponentView.js
+++ js/views/editor/sidebar/properties/components/PropertiesComponentView.js
@@ -12,10 +12,32 @@ Backbone
     var propComponentsView = Backbone.View.extend({
         id: 'viewDomId',
         className: 'viewClassName',
-        
+        components: [],
+        //Expected format of single component
+        compDump: {
+        	number: 1,
+        	name: 'Physics',
+        	sortable: 'true',
+        	properties: [
+        		{
+        			name : 'Movement',
+        			type : 'bool',
+        			value : true
+        		}
+        	],
+        },
+        setComponents: function() {
+        	//Clean up old
+        	//
+        },
+
         render: function(){
+        	// for (var i = 0; i < components.length; i++) {
+        	// 	components[i].
+        	// }
             return this;
         },
+
     });
     return propComponentsView;   
 });
