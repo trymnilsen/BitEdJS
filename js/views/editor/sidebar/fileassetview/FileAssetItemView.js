2484f1ec9ea138105635f00001f44aba93001537
Trym Nilsen <trym.nilsen@gmail.com>
2014-11-30 02:09:08 +0100

    Added asset dragging

--- js/views/editor/sidebar/fileassetview/FileAssetItemView.js
+++ js/views/editor/sidebar/fileassetview/FileAssetItemView.js
@@ -4,31 +4,79 @@ define([
   'jquery',
   'underscore',
   'backbone',
+  'eventor',
   'text!views/editor/sidebar/fileassetview/FileAssetItemTemplate.html',
   'bootstrapSweetAlert',
   'jqueryui'
-], function($, _, Backbone, ItemTemplate){
+], function($, _, Backbone, eventor, ItemTemplate){
 
     var FileItemView = Backbone.View.extend({
-
+        /**
+         * Backbone property, sets the the type of element for this
+         * views DOM element
+         * @type {String}
+         */
         tagName:  'li',
+        /**
+         * Backbone property, sets the classname(s) of this
+         * views DOM element
+         * @type {String}
+         */
         className: 'file-asset-item-wrapper',
+        /**
+         * templating function for returning fresh html with our passed data
+         * @type {function(Object):string}
+         */
         template: _.template(ItemTemplate),
-     
+        /**
+         * The options for the dragging functionallity of this view
+         * @type {Object}
+         */
+        draggableOptions:  {
+            cursor: "move",
+            cursorAt: { 
+                top: 0, 
+                left: 0 
+            },
+            helper: function() {
+                return $( "<i class='fa fa-fx fa-image drag-asset-preview'></i>" );
+            },
+            start: function() {
+                eventor.trigger('editor.asset.drag.start');
+            },
+            stop: function() {
+                eventor.trigger('editor.asset.drag.stop');
+            }
+        },
+        /**
+         * Backbone property, hash of delegated events for this view
+         * @type {Object}
+         */
         events: {
             'click .fa-times'   : 'promptDelete'
         },
-
+        /**
+         * Called on creation, initializes this view
+         */
         initialize: function() {
             this.listenTo(this.model, 'change', this.render);
             this.listenTo(this.model, 'destroy', this.remove);
         },
-
+        /**
+         * Sets up dom elements and adds DOM specific behaviour
+         * @return {Object} returns self for fluency
+         */
         render: function() {
             var templateData = this.model.toJSON();
             this.$el.html(this.template(templateData));
+            //Add draggable functionality
+            //Todo, add draggable on finished uploading
+            this.$el.draggable(this.draggableOptions);
             return this;
         },
+        /**
+         * Callbakc for remove asset button, prompts the user with a popup
+         */
         promptDelete: function()
         {
             swal({
@@ -43,6 +91,10 @@ define([
             },_.bind(this.deletePromtResponse,this));
             //
         },
+        /**
+         * Reponse for item deleted
+         * @param  {Boolean} isConfirm did the user confirm delete (true)
+         */
         deletePromtResponse: function(isConfirm)
         {
             if (isConfirm)
@@ -55,14 +107,19 @@ define([
                 swal("Cancelled", "Your asset file has not been deleted :)", "error");
             }
         },
+        /**
+         * Removes this view
+         */
         remove: function()
         {
             this.$el.trigger('remove');
         },
+        /**
+         * Not sure if this is used?
+         */
         clear: function() {
             this.model.destroy();
         }
-
     });
 
     return FileItemView;
--- js/views/editor/sidebar/properties/components/PropertiesComponentParameterList.js
+++ js/views/editor/sidebar/properties/components/PropertiesComponentParameterList.js
@@ -33,39 +33,64 @@ define([
         parameters: [],
         /**
          * does the component have droppable params?
-         * @type {Boolean}
+         * @type {Integer}
          * @readOnly
          */
-        canDrop   : false,
+        canDrop   : 0,
+        /**
+         * Called as constructor, initalizes the view
+         * @param  {Object} options the parameteres for this view
+         */
         initialize: function(options) {
 
             this.processLoadedParameterViews();
             this.parameters = options;
+
+            this.listenTo(eventor, 'editor.asset.drag.start', this.highlightDropZone);
+            eventor.on('editor.asset.drag.stop',
+                                    _.bind(this.unHighlightDropZone,this));
         },
+        /**
+         * Sets up the DOM nodes and handles behaviour for the DOM
+         * @return {[type]} [description]
+         */
         render: function() {
             for (var i = 0; i < this.parameters.length; i++) {
                 //Create the param
                 var paramView = this.renderParameter(this.parameters[i]);
                 this.$el.append(paramView.el);
-            };
+            }
+            //If we have drop behaviour on a param, activate it
+            if(this.canDrop > 0)
+            {
+                this.$el.droppable();
+            }
             return this;
         },
+        /**
+         * Sets up a Dom node with view for a given parameter
+         * @param  {Object} parameter
+         * @return {Object | null} ParameterView
+         */
         renderParameter: function(parameter) {
-            var parameterViewConstructor = this.viewTypes[parameter.type];
-            if(parameterViewConstructor === undefined)
+            var ParameterViewConstructor = this.viewTypes[parameter.type];
+            if(ParameterViewConstructor === undefined)
             {
                 return null;
             }
             //If the param is droppable we enable highlighting of all of the
             //component, when dragging starts
-            if(parameterViewConstructor.DROPPABLE)
+            if(ParameterViewConstructor.DROPPABLE)
             {
-                this.canDrop = true;
+                this.canDrop++;
             }
-            var parameterView = new parameterViewConstructor(parameter);
+            var parameterView = new ParameterViewConstructor(parameter);
 
             return parameterView.render();
         },
+        /**
+         * Internal method for processing the requirejs loaded views
+         */
         processLoadedParameterViews: function()
         {
             var lw = PropertiesComponentParameterList.LOADEDVIEWS;
@@ -75,7 +100,37 @@ define([
                 {
                     this.viewTypes[type] = lw[i];
                 }
-            };
+            }
+        },
+        /**
+         * Callback on a dragged item hovering over this parameter list
+         */
+        highlightDropOver: function()
+        {
+
+        },
+        /**
+         * Callback on a dragged item dragged out of this parameter list area
+         */
+        highlightDropOut: function()
+        {
+
+        },
+        /**
+         * Callback triggered when a draggable that can be dropped on one of the
+         * params in this list has started dragging
+         */
+        highlightDropZone: function()
+        {
+            console.log('highlightDropzone');
+        },
+        /**
+         * Callback triggered when a draggable that can be dropped on one of the
+         * params in this list has ended dragging
+         */
+        unHighlightDropZone: function()
+        {
+            console.log('unhighlight dropzone');
         }
     });
 
